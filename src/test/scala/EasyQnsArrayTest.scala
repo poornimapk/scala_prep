01a33f8f6ee7e1f954588ff85e2d4b3441ac0bf2
Poornima P K <poornima.pk@gmail.com>
2024-05-27 14:38:13 -0700

    Added more unit tests

--- src/test/scala/EasyQnsArrayTest.scala
+++ src/test/scala/EasyQnsArrayTest.scala
@@ -2,9 +2,11 @@ import easy.EasyQnsArrays
 import org.scalatest.funsuite.AnyFunSuite
 
 class EasyQnsArrayTest extends AnyFunSuite {
-  test("Remove element from array of Integers") {
-    val easyQnsArray = new EasyQnsArrays()
+  val easyQnsArray = new EasyQnsArrays()
+  test("Remove element from array of Integers, element exists") {
     assert(easyQnsArray.removeElement(Array(2, 4, 5, 2, 5, 6, 7), 5) === Array(2, 4, 2, 6, 7))
   }
-
+  test("Remove element from array of Integers, element doesn't exists") {
+    assert(easyQnsArray.removeElement(Array(2, 4, 5, 2, 5, 6, 7), 9) === Array(2, 4, 5, 2, 5, 6, 7))
+  }
 }
